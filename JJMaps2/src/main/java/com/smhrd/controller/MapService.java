a0902e88d1ef084acae21816eddf6a7190198c2d
smhrd <smhrd@59.0.124.54>
2023-01-04 18:52:30 +0900

    3번째 커밋 : store dao, mapper , mapservice(servlet)

--- JJMaps2/src/main/java/com/smhrd/controller/MapService.java
+++ JJMaps2/src/main/java/com/smhrd/controller/MapService.java
@@ -1,11 +1,16 @@
 package com.smhrd.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.smhrd.model.StoreDAO;
+import com.smhrd.model.StoreVO;
 
 
 public class MapService extends HttpServlet {
@@ -13,7 +18,9 @@ public class MapService extends HttpServlet {
 
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		StoreDAO dao = new StoreDAO();
+		String store_cate = request.getParameter("store_cate");
+		ArrayList<StoreVO> S_List = dao.storeCateSelect(store_cate);
 	}
 
 }
--- JJMaps2/src/main/java/com/smhrd/model/StoreDAO.java
+++ JJMaps2/src/main/java/com/smhrd/model/StoreDAO.java
@@ -1,5 +1,21 @@
 package com.smhrd.model;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.smhrd.db.SqlSessionManager;
+
 public class StoreDAO {
+	private SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+	private SqlSession session;
+	public ArrayList<StoreVO> storeCateSelect(String store_cate) {
+		session = sqlSessionFactory.openSession(true);
+		List<StoreVO> list =session.selectList("StoreSelect");
+		session.close();
+		
+		return (ArrayList<StoreVO>)list;
+	}
 	
 }
