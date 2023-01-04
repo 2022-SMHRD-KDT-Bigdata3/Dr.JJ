9f0eea808c57e9df6a0f5a50a56e15f5d8b0f0c6
smhrd <smhrd@59.0.124.54>
2023-01-04 18:44:04 +0900

    2번째 커밋 : java 모델 생성 및 mapper 수정

---
+++ JJMaps2/src/main/java/com/smhrd/model/ReserveDetailsVO.java
@@ -0,0 +1,58 @@
+package com.smhrd.model;
+
+//예약 상세
+public class ReserveDetailsVO {
+
+ // 예약상세 순번
+ private Long rdId;
+
+ // 메뉴 아이디
+ private Double menuId;
+
+ // 예약 번호
+ private Long rNumber;
+
+ // 주문 수량
+ private String menuCnt;
+
+ public Long getRdId() {
+     return rdId;
+ }
+
+ public void setRdId(Long rdId) {
+     this.rdId = rdId;
+ }
+
+ public Double getMenuId() {
+     return menuId;
+ }
+
+ public void setMenuId(Double menuId) {
+     this.menuId = menuId;
+ }
+
+ public Long getRNumber() {
+     return rNumber;
+ }
+
+ public void setRNumber(Long rNumber) {
+     this.rNumber = rNumber;
+ }
+
+ public String getMenuCnt() {
+     return menuCnt;
+ }
+
+ public void setMenuCnt(String menuCnt) {
+     this.menuCnt = menuCnt;
+ }
+
+ // ReservDetails 모델 복사
+ public void CopyData(ReserveDetailsVO param)
+ {
+     this.rdId = param.getRdId();
+     this.menuId = param.getMenuId();
+     this.rNumber = param.getRNumber();
+     this.menuCnt = param.getMenuCnt();
+ }
+}
