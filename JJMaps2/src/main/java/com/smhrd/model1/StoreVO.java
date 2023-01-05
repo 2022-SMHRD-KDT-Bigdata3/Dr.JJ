1649ddf327deab3e1cbde0520457bcc832ae9ab9
smhrd <smhrd@59.0.124.54>
2023-01-05 20:39:59 +0900

    음음 오키오키 STOREVO 수정

--- JJMaps2/src/main/java/com/smhrd/model1/StoreVO.java
+++ JJMaps2/src/main/java/com/smhrd/model1/StoreVO.java
@@ -2,119 +2,141 @@ package com.smhrd.model1;
 
 public class StoreVO {
 
-	    private String storeId;
+	private String storeId;
 
-	    private String storeName;
+    private String storeName;
 
-	    private String storeAddr;
+    private String storeAddr;
 
-	    private String storeTel;
+    private String storeTel;
 
-	    private String storeOpenDt;
+    private String storeOpenDt;
 
-	    private String storeCloseDt;
+    private String storeCloseDt;
 
-	    private Integer storeCardYn;
+    private Integer storeCardYn;
 
-	    private Integer storeStatus;
+    private Integer storeStatus;
 
-	    private String userId;
+    private String userId;
 
-	    private String storeCate;
-
-	    public String getStoreId() {
-	        return storeId;
-	    }
-
-	    public void setStoreId(String storeId) {
-	        this.storeId = storeId;
-	    }
-
-	    public String getStoreName() {
-	        return storeName;
-	    }
-
-	    public void setStoreName(String storeName) {
-	        this.storeName = storeName;
-	    }
-
-	    public String getStoreAddr() {
-	        return storeAddr;
-	    }
-
-	    public void setStoreAddr(String storeAddr) {
-	        this.storeAddr = storeAddr;
-	    }
-
-	    public String getStoreTel() {
-	        return storeTel;
-	    }
-
-	    public void setStoreTel(String storeTel) {
-	        this.storeTel = storeTel;
-	    }
-
-	    public String getStoreOpenDt() {
-	        return storeOpenDt;
-	    }
-
-	    public void setStoreOpenDt(String storeOpenDt) {
-	        this.storeOpenDt = storeOpenDt;
-	    }
-
-	    public String getStoreCloseDt() {
-	        return storeCloseDt;
-	    }
-
-	    public void setStoreCloseDt(String storeCloseDt) {
-	        this.storeCloseDt = storeCloseDt;
-	    }
-
-	    public Integer getStoreCardYn() {
-	        return storeCardYn;
-	    }
-
-	    public void setStoreCardYn(Integer storeCardYn) {
-	        this.storeCardYn = storeCardYn;
-	    }
-
-	    public Integer getStoreStatus() {
-	        return storeStatus;
-	    }
-
-	    public void setStoreStatus(Integer storeStatus) {
-	        this.storeStatus = storeStatus;
-	    }
-
-	    public String getUserId() {
-	        return userId;
-	    }
-
-	    public void setUserId(String userId) {
-	        this.userId = userId;
-	    }
-
-	    public String getStoreCate() {
-	        return storeCate;
-	    }
-
-	    public void setStoreCate(String storeCate) {
-	        this.storeCate = storeCate;
-	    }
-
-	    // Stores 모델 복사
-	    public void CopyData(StoreVO param)
-	    {
-	        this.storeId = param.getStoreId();
-	        this.storeName = param.getStoreName();
-	        this.storeAddr = param.getStoreAddr();
-	        this.storeTel = param.getStoreTel();
-	        this.storeOpenDt = param.getStoreOpenDt();
-	        this.storeCloseDt = param.getStoreCloseDt();
-	        this.storeCardYn = param.getStoreCardYn();
-	        this.storeStatus = param.getStoreStatus();
-	        this.userId = param.getUserId();
-	        this.storeCate = param.getStoreCate();
-	    }
+    private String storeCate;
+    private Double Latitude;
+    private Double Longitude;   
+    public StoreVO() {
+    }
+    
+	public StoreVO(String storeId, String storeName, String storeAddr, String storeTel, String storeOpenDt,
+			String storeCloseDt, Integer storeCardYn, Integer storeStatus, String userId, String storeCate,
+			Double latitude, Double longitude) {
+	
+		this.storeId = storeId;
+		this.storeName = storeName;
+		this.storeAddr = storeAddr;
+		this.storeTel = storeTel;
+		this.storeOpenDt = storeOpenDt;
+		this.storeCloseDt = storeCloseDt;
+		this.storeCardYn = storeCardYn;
+		this.storeStatus = storeStatus;
+		this.userId = userId;
+		this.storeCate = storeCate;
+		Latitude = latitude;
+		Longitude = longitude;
 	}
+		public StoreVO(String storeId, String storeName, String storeAddr, String storeTel, String storeOpenDt,
+			String storeCloseDt, Integer storeCardYn, Integer storeStatus, String userId, String storeCate) {
+		super();
+		this.storeId = storeId;
+		this.storeName = storeName;
+		this.storeAddr = storeAddr;
+		this.storeTel = storeTel;
+		this.storeOpenDt = storeOpenDt;
+		this.storeCloseDt = storeCloseDt;
+		this.storeCardYn = storeCardYn;
+		this.storeStatus = storeStatus;
+		this.userId = userId;
+		this.storeCate = storeCate;
+	}
+		
+		public String getStoreId() {
+			return storeId;
+		}
+		public void setStoreId(String storeId) {
+			this.storeId = storeId;
+		}
+		public String getStoreName() {
+			return storeName;
+		}
+		public void setStoreName(String storeName) {
+			this.storeName = storeName;
+		}
+		public String getStoreAddr() {
+			return storeAddr;
+		}
+		public void setStoreAddr(String storeAddr) {
+			this.storeAddr = storeAddr;
+		}
+		public String getStoreTel() {
+			return storeTel;
+		}
+		public void setStoreTel(String storeTel) {
+			this.storeTel = storeTel;
+		}
+		public String getStoreOpenDt() {
+			return storeOpenDt;
+		}
+		public void setStoreOpenDt(String storeOpenDt) {
+			this.storeOpenDt = storeOpenDt;
+		}
+		public String getStoreCloseDt() {
+			return storeCloseDt;
+		}
+		public void setStoreCloseDt(String storeCloseDt) {
+			this.storeCloseDt = storeCloseDt;
+		}
+		public Integer getStoreCardYn() {
+			return storeCardYn;
+		}
+		public void setStoreCardYn(Integer storeCardYn) {
+			this.storeCardYn = storeCardYn;
+		}
+		public Integer getStoreStatus() {
+			return storeStatus;
+		}
+		public void setStoreStatus(Integer storeStatus) {
+			this.storeStatus = storeStatus;
+		}
+		public String getUserId() {
+			return userId;
+		}
+		public void setUserId(String userId) {
+			this.userId = userId;
+		}
+		public String getStoreCate() {
+			return storeCate;
+		}
+		public void setStoreCate(String storeCate) {
+			this.storeCate = storeCate;
+		}
+		public Double getLatitude() {
+			return Latitude;
+		}
+		public void setLatitude(Double latitude) {
+			Latitude = latitude;
+		}
+		public Double getLongitude() {
+			return Longitude;
+		}
+		public void setLongitude(Double longitude) {
+			Longitude = longitude;
+		}
+		@Override
+		public String toString() {
+			return "StoreVO [storeId=" + storeId + ", storeName=" + storeName + ", storeAddr=" + storeAddr
+					+ ", storeTel=" + storeTel + ", storeOpenDt=" + storeOpenDt + ", storeCloseDt=" + storeCloseDt
+					+ ", storeCardYn=" + storeCardYn + ", storeStatus=" + storeStatus + ", userId=" + userId
+					+ ", storeCate=" + storeCate + ", Latitude=" + Latitude + ", Longitude=" + Longitude + "]";
+		}
 
+	    
+}
--- JJMaps2/src/main/webapp/GoogleMapTest.jsp
+++ JJMaps2/src/main/webapp/GoogleMapTest.jsp
@@ -1,22 +1,90 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"
-    import="com.smhrd.db1.*,java.util.*, java.sql.*"%>
+	pageEncoding="UTF-8" import="com.smhrd.db1.*,java.util.*, java.sql.*"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
-	<style>
-		body{
-			width: 100%; height: 100%;
-		}
-		#map, #map2{
-			width: 1000px;
-			height :  700px;
-		}
-	</style>
+<style>
+body {
+	width: 100%;
+	height: 100%;
+}
+
+#map, #map2 {
+	width: 1000px;
+	height: 700px;
+}
+</style>
 </head>
 <body>
 	<h1>DB좌표 받아오기</h1>
+	<div id="locationField">
+		<input id="autocomplete" placeholder="Enter your address" type="text">
+	</div>
+
+	<input class="field" id="lat" />
+	<input class="field" id="lng" />
+	<div id="map"></div>
+	<hr>
+	<script
+		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&libraries=places&callback=initAutocomplete"
+		async defer></script>
+	<script type="text/javascript">
+		var placeSearch, autocomplete;
+		function initAutocomplete() {
+			// Create the autocomplete object, restricting the search to geographical
+			// location types.
+			autocomplete = new google.maps.places.Autocomplete((document
+					.getElementById('autocomplete')), {
+				types : [ 'geocode' ]
+			});
+			// When the user selects an address from the dropdown, populate the address
+			// fields in the form.
+			autocomplete.addListener('place_changed', fillInAddress);
+		}
+		function fillInAddress() {
+			// Get the place details from the autocomplete object.
+			var place = autocomplete.getPlace();
+			document.getElementById("lat").value = place.geometry.location
+					.lat();
+			document.getElementById("lng").value = place.geometry.location
+					.lng();
+		}
+		var customLabel = {
+			A01 : {
+				label : 'R'
+			},
+			A02 : {
+				label : 'B'
+			},
+			A07 : {
+				label : 'C'
+			}
+
+		};
+		function initMap() {
+			var map = new google.maps.Map(document.getElementById('mal'), {
+				center : new google.maps.LanLng(33.2504067, 126.563636),
+				zoom : 11
+			});
+			var infoWindow = new google.mapsInfoWindow;
+
+			downloadUrl('http://localhost:8081/JJMap/output.xml',
+					function(data) {
+						var xml = data.responseXML;
+						var markers = xml.documentElement
+								.getElementsByTagName('marker');
+						Array.prototype.forEach.call(markers,
+								function(markerElem) {
+									var store_name = markerElem
+											.getAttribute('store_name');
+									var address = markerElem
+											.getAttribute('store_addr');
+									var pint = new google.mpasLatLng()
+								})
+					})
+		}
+	</script>
 </body>
 </html>
