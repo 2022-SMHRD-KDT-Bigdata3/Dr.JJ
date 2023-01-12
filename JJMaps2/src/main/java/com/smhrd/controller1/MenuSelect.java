f896f87ffce68899f2a2472eb618ee941915d700
smhrd <smhrd@218.157.24.61>
2023-01-12 17:46:55 +0900

    로그인 수정

--- JJMaps2/src/main/java/com/smhrd/controller1/MenuSelect.java
+++ JJMaps2/src/main/java/com/smhrd/controller1/MenuSelect.java
@@ -14,26 +14,24 @@ import com.google.gson.Gson;
 import com.smhrd.model1.MenuDAO;
 import com.smhrd.model1.MenuVO;
 
-/**
- * Servlet implementation class MenuSelect
- */
+
 public class MenuSelect extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+       //점포 상세페이지에서 연결하기 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		MenuDAO dao = new MenuDAO();
-		String store_id = (String) session.getAttribute("store_id");
-		ArrayList<MenuVO> list =dao.menuSelect(store_id);
-		for(MenuVO vo : list) {
-			System.out.println(vo.toString());
-		}
-		response.setCharacterEncoding("UTF-8");
-		PrintWriter out = response.getWriter();
-		Gson gson = new Gson();
-		String result = gson.toJson(list);
-		System.out.println(result);
-		out.print(result);
-		response.sendRedirect("Mystore.jsp");
+//		HttpSession session = request.getSession();
+//		MenuDAO dao = new MenuDAO();
+//		Integer store_id =  Integer.parseInt(session.getAttribute("store_id"));
+//		ArrayList<MenuVO> list =dao.menuSelect(store_id);
+//		for(MenuVO vo : list) {
+//			System.out.println(vo.toString());
+//		}
+//		response.setCharacterEncoding("UTF-8");
+//		PrintWriter out = response.getWriter();
+//		Gson gson = new Gson();
+//		String result = gson.toJson(list);
+//		System.out.println(result);
+//		out.print(result);
+//		response.sendRedirect("Mystore.jsp");
 	}
 }
